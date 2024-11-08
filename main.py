f4e9ea55a51391b46601e27a6e42f21303f0060f
Kaspatcho <nosilaw7777@gmail.com>
2024-11-07 23:55:32 -0300

    removing test file

--- main.py
+++
@@ -1,6 +0,0 @@
-from requests import Session
-
-s = Session()
-lines = s.get('https://rest-emtu.noxxonsat.com.br/rest/lineDetails?linha=285', verify=False).json()['linhas'][0]['veiculos']
-
-print(lines)
